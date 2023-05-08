58d120dbfed83111442e1c5434a83a56555a9434
a0lim <a0lim816@gmail.com>
2023-05-08 21:37:26 +0900

    [feat] create Hello.java

---
+++ src/section0/Hello.java
@@ -0,0 +1,7 @@
+package section0;
+
+public class Hello {
+    public static void main(String[] args){ // main method: 프로그램의 시작점
+        System.out.println("Hello"); // System: class / out: field / println: method
+    }
+}
