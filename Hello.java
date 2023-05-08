f942f67bc242750c05307077573e6cd4777eb118
a0lim <a0lim816@gmail.com>
2023-05-08 23:32:06 +0900

    [feat] git push 취소

--- Hello.java
+++
@@ -1,5 +0,0 @@
-public class Hello{ // class 이름(Hello) = 파일 이름
-    public static void main(String[] args){ // main method: 프로그램의 시작점
-        System.out.println("Hello"); // System: class / out: field / println: method
-    }
-}
