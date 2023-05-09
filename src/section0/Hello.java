a781063d480f2bae5cb4897026a09a29141db04c
a0lim <a0lim816@gmail.com>
2023-05-09 10:50:25 +0900

    feat: test

--- src/section0/Hello.java
+++ src/section0/Hello.java
@@ -3,6 +3,7 @@ package section0;
 public class Hello { // class 이름(Hello) = 파일 이름
     public static void main(String[] args){ // main method: 프로그램의 시작점
         System.out.println("Hello"); // System: class / out: field / println: method
+        System.out.println("test");
 
     }
 }
